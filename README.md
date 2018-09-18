55affa9a4f3561a1bd0b7689e9c4b4f7e1307039
Chris Crane <chris.crane@bellaliant.net>
2018-09-18 00:20:22 -0230

    Adds profile image and introduction

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Project 1
 + By: Chris Crane
-+ Production URL: <http://p1.chriscrane-dwa.com>
++ Production URL: http://p1.chriscrane-dwa.com
 
 ## Outside resources
-*your list of outside resources go here*
+* [jQuery 3.1.1](https://jquery.com/download/)
+* [Bootstrap 4.1.3](https://getbootstrap.com/docs/4.1/getting-started/introduction/)
 
 ## Notes for instructor
 *any notes for me to refer to while grading; if none, omit this section*
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Chris Crane</title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/main.css" />
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-6">
+                    <h1 class="text-center">Chris Crane</h1>
+                    <p>
+                        <img id="imgProfilePhoto" src="/images/Chris_Crane_Universal.jpg" class="img-fluid rounded shadow mx-auto d-block" alt="Chris Crane with wife at Universal Studios" />
+                    </p>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-6">
+                    <h2 class="text-center">About Me</h2>
+                    <p class="text-justify">
+                        I living in Newfoundland, Canada, with my wife and 3 kids. For the past 5 years I have been employed as
+                        Manager of IT for the Newfoundland and Labrador English School District, and as a Senior Systems Analyst
+                        for 13 years prior to that. What seems like ages ago I completed my Bachelors Degree in Computer Science
+                        and Applied Mathematics.
+                    </p>
+                    <p class="text-justify">
+                        I am really looking forward to interacting with all of you over the next few months as we work our way
+                        though the course.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- jQuery JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <!-- Bootstrap JS -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
+    </body>
+</html>
